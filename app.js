56d36352d9c55fc80818df20c0bbbfb7b458f0a3
Catarina Laranjeiro <ctlaranjeiro@gmail.com>
2020-09-20 23:42:20 +0100

    adds MONGODB_URI

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const passport     = require('passport');
 require('./configs/passport');
 
 mongoose
-  .connect('mongodb://localhost/todos-app', {
+  .connect(process.env.MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
