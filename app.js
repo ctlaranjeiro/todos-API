f6b2569a9f95c41bd3a015e05a65ea8f6d9e46a8
Catarina Laranjeiro <ctlaranjeiro@gmail.com>
2020-09-27 19:59:15 +0100

    sets cookie sameSite false

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use(session ({
   resave: true,
   saveUnitialized: true,
   rolling: true,
-  cookie: { expires: 60000, sameSite: 'strict' }
+  cookie: { expires: 60000, sameSite: false }
 }));
 
 // PASSPORT SETTINGS
